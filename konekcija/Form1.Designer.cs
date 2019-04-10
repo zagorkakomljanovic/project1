61700c0a83bb8fe0a69cae7458734070843c80eb
miskol85 <48725345+miskol85@users.noreply.github.com>
2019-04-10 14:44:26 +0200

    Prom

--- konekcija/Form1.Designer.cs
+++ konekcija/Form1.Designer.cs
@@ -30,13 +30,15 @@
         {
             this.Button1 = new System.Windows.Forms.Button();
             this.labela = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Button1
             // 
-            this.Button1.Location = new System.Drawing.Point(77, 63);
+            this.Button1.Location = new System.Drawing.Point(103, 78);
+            this.Button1.Margin = new System.Windows.Forms.Padding(4);
             this.Button1.Name = "Button1";
-            this.Button1.Size = new System.Drawing.Size(126, 46);
+            this.Button1.Size = new System.Drawing.Size(168, 57);
             this.Button1.TabIndex = 0;
             this.Button1.Text = "Button1";
             this.Button1.UseVisualStyleBackColor = true;
@@ -44,19 +46,32 @@
             // labela
             // 
             this.labela.AutoSize = true;
-            this.labela.Location = new System.Drawing.Point(89, 167);
+            this.labela.Location = new System.Drawing.Point(119, 206);
+            this.labela.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labela.Name = "labela";
-            this.labela.Size = new System.Drawing.Size(35, 13);
+            this.labela.Size = new System.Drawing.Size(46, 17);
             this.labela.TabIndex = 2;
             this.labela.Text = "label1";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(165, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(379, 321);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.labela);
             this.Controls.Add(this.Button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -68,6 +83,7 @@
 
         private System.Windows.Forms.Button Button1;
         private System.Windows.Forms.Label labela;
+        private System.Windows.Forms.Button button2;
     }
 }
 
